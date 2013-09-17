3125095a7e8b8a1322c8fb5e65512f3bc4f1d7c9
eze-eoc <eai@eoconsulting.com.ar>
2013-09-17 10:20:34 -0300

    Small change in headers

--- src/RepetierHost/model/BasicList.cs
+++ src/RepetierHost/model/BasicList.cs
@@ -13,7 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 
-   written by eze-eoc at kikai labs (eai@eoconsulting.com.ar)
+   written by eze-eoc
 */
 
 using System;
